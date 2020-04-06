5ff7f1f436aca690b4fe94e7389205be21d11179
Semenchik-Polina <semenchik.polina@mail.ru>
2020-04-06 16:50:07 +0300

    Add Ninject IoC

---
+++ LogSystem.PL/Utils/NinjectDependencyResolver.cs
@@ -0,0 +1,40 @@
+﻿using LogSystem.BLL.Interfaces;
+using LogSystem.BLL.Services;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace LogSystem.PL.Utils
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            _kernel = kernelParam;
+            AddBindings();
+        }
+
+
+        private void AddBindings()
+        {
+            _kernel.Bind<IUserService>().To<UserService>();
+            _kernel.Bind<IValidationService>().To<ValidationService>();
+            _kernel.Bind<IUserActionService>().To<UserActionService>();
+        }
+
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+    }
+}
