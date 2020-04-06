656a9ded5b44b6d98cf59b260df73e66a192c614
Semenchik-Polina <semenchik.polina@mail.ru>
2020-04-06 16:55:53 +0300

    Add authorization (filter, MainController)

---
+++ LogSystem.PL/Controllers/MainController.cs
@@ -0,0 +1,54 @@
+﻿using LogSystem.Common.Enums;
+using System;
+using System.Web.Mvc;
+using LogSystem.BLL.Utils;
+using System.Web.Security;
+using LogSystem.PL.Filters;
+using LogSystem.BLL.Interfaces;
+using System.Threading.Tasks;
+
+namespace LogSystem.PL.Controllers
+{
+    [UserAuthorize]
+    public class MainController : Controller
+    {
+        private IUserService UserService { get; set; }
+
+        public MainController(IUserService userService)
+        {
+            UserService = userService;
+        }
+
+        [HttpGet]
+        public ActionResult GetUserProfile()
+        {
+            int id = Int32.Parse(Request.Cookies[UserCookieHelper.userCookieName]["UserID"]);
+            var userType = Request.Cookies[UserCookieHelper.userCookieName]["UserType"];
+            if (Enum.IsDefined(typeof(UserType), userType))
+            {
+                return RedirectToAction("Edit", "User", new { id });
+            }
+            else
+            {
+                ModelState.AddModelError("Profile", "No profile information found");
+                return RedirectToAction("LogIn", "LogIn");
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> SignOut()
+        {
+            int id = Int32.Parse(Request.Cookies[UserCookieHelper.userCookieName]["UserID"]);
+            await UserService.LogOut(id);
+
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            Response.Cookies[FormsAuthentication.FormsCookieName].Expires = DateTime.Now.AddYears(-1);
+
+            UserCookieHelper.DeleteUserCookie(Response.Cookies[UserCookieHelper.userCookieName]);
+
+            return Redirect("../LogIn/LogIn");
+        }
+
+    }
+}
---
+++ LogSystem.PL/Filters/UserAuthorizeAttribute.cs
@@ -0,0 +1,57 @@
+﻿using LogSystem.BLL.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LogSystem.PL.Filters
+{
+    public class UserAuthorizeAttribute : AuthorizeAttribute
+    {
+        private string[] allowedTypes = new string[] { };
+
+        public UserAuthorizeAttribute()
+        { }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (AuthorizeCore(filterContext.HttpContext))
+            {
+                base.OnAuthorization(filterContext);
+            }
+            else
+            {
+                filterContext.Result = new RedirectResult("~/LogIn/LogIn");
+            }
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (!String.IsNullOrEmpty(base.Roles))
+            {
+                allowedTypes = base.Roles.Split(new char[] { ',' });
+                for (int i = 0; i < allowedTypes.Length; i++)
+                {
+                    allowedTypes[i] = allowedTypes[i].Trim();
+                }
+            }
+            var str = httpContext.Request.Cookies[UserCookieHelper.userCookieName]["UserID"];
+            return httpContext.Request.IsAuthenticated && Type(httpContext) && str != null;
+        }
+
+        private bool Type(HttpContextBase httpContext)
+        {
+            if (allowedTypes.Length > 0)
+            {
+                for (int i = 0; i < allowedTypes.Length; i++)
+                {
+                    if (Array.IndexOf(allowedTypes, httpContext.Request.Cookies[UserCookieHelper.userCookieName]["UserType"]) > -1)
+                        return true;
+                }
+                return false;
+            }
+            return true;
+        }
+    }
+}
